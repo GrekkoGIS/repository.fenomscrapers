ba1cad685d7524f2ddc641ae33c617ceeb6b8186
123Venom <46383815+123Venom@users.noreply.github.com>
2020-09-25 03:23:09 -0400

    Delete xcopy-venom.bat

--- xcopy-venom.bat
+++
@@ -1,1 +0,0 @@
-xcopy "C:\Users\Ken Brown\AppData\Roaming\Kodi\addons\script.module.fenomscrapers" "C:\Users\Ken Brown\Desktop\Fenom Scrapers\repository.fenomscrapers\script.module.fenomscrapers\" /c /e /h /i /q /r /y
